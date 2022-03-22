9fbe08b6d61c7d2139e7b937e4fddf0c461ec66f
TheComputerWizardLizard <antrussell2004.personal@gmail.com>
2022-03-22 12:41:19 -0700

    general makeover

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -1,9 +1,9 @@
 const contractAddress = "0x6A6bB203112BFd2c5CfBfBB4A728F67E879EAAe1"; //Replace with your own contract address
 const chain = 'polygon'; // rinkeby or polygon
 
-const welcome_h1 = "Welcome to the CodeCats NFT Project!!";
+const welcome_h1 = "Welcome to the America First NFT Campaign!";
 const welcome_h2 = "Connect to MetaMask to Get Started";
-const welcome_p = 'The CodeCats NFT Project is a decentralized, open-source project that aims to demonstrate how to develope and launch your own NFT Collection. Follow step by step on the <a href="https://youtube.com/codestackr"  target="_blank" rel="noopener noreferrer">codeSTACKr</a> YouTube channel.';
+const welcome_p = 'The America First is a movement that is aimed at supporting all members of America First, and to support any american who has had their 1st amendment right violated trying to express their right to free speech!';
 const h1_presale_coming_soon = "NFT Drop Coming Soon!!";
 const h1_presale_mint = "Pre-Sale Minting Open!!";
 const h1_public_mint = "Public Minting Open!!";
