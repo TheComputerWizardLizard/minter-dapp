705a87be763881ab31e4aecbd3b02269e8b35274
TheComputerWizardLizard <antrussell2004.personal@gmail.com>
2022-03-22 11:54:40 -0700

    America First Collection

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -1,5 +1,5 @@
-const contractAddress = "YOUR CONTRACT ADDRESS"; //Replace with your own contract address
-const chain = 'rinkeby'; // rinkeby or polygon
+const contractAddress = "0x6A6bB203112BFd2c5CfBfBB4A728F67E879EAAe1"; //Replace with your own contract address
+const chain = 'polygon'; // rinkeby or polygon
 
 const welcome_h1 = "Welcome to the CodeCats NFT Project!!";
 const welcome_h2 = "Connect to MetaMask to Get Started";
